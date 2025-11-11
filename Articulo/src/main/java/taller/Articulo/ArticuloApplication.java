25449ad3e2fff4cf848ea9f56f7f1f8a7f7f772d
RonaldoSol7 <132590908+Ronaldo129@users.noreply.github.com>
2025-11-11 18:19:55 -0500

    articulo

---
+++ Articulo/src/main/java/taller/Articulo/ArticuloApplication.java
@@ -0,0 +1,13 @@
+package taller.Articulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticuloApplication.class, args);
+	}
+
+}
